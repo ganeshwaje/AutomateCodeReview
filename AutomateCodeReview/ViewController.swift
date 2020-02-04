8565957f8d18db9653d7bd9a960d5773a991f3f1
Ganesh Waje <ganesh.waje@outlook.com>
2020-02-04 21:04:09 +0530

    Initial Commit

---
+++ AutomateCodeReview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutomateCodeReview
+//
+//  Created by Ganesh Waje on 04/02/20.
+//  Copyright © 2020 Ganesh Waje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
